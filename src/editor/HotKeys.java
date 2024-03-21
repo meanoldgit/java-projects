01df4d24186f226808b6d4a9a4d1cc5f53146e5e
meanoldgit <alexantonaranda@gmail.com>
2024-03-21 21:11:34 +0100

    hotkeys change

--- src/editor/HotKeys.java
+++ src/editor/HotKeys.java
@@ -1,17 +1,53 @@
 package editor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-public class HotKeys {
+class HotKeys implements KeyListener {
     Cursor cursor = new Cursor();
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_B:
+        System.out.println("bbc");
+        break;
+
+        default:
+        break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+
+    // public void KeyPressed(KeyEvent e) {
+
+    //     switch (e.getKeyCode()) {
+    //         case KeyEvent.VK_B:
+    //         System.out.println("bbc");
+    //         break;
+
+    //         default:
+    //         break;
+    //     }
+    // }
 
     public void close(ArrayList<ArrayList<Character>> lines) {
         cursor.changeColorWhite();
         cursor.clearScreenAfterCursor();
         System.out.println("\nbye");
 
-        for (int i = 0; i < lines.size(); i++)
+        for (int i = 0; i < lines.size(); i++) {
             lines.get(i).add('\n');
+        }
 
         // Print lines.
         for (int i = 0; i < lines.size(); i++)
